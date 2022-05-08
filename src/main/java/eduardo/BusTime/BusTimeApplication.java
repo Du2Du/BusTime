6bb7da850e2aa102ba4024165c566d59ebedf6d5
Du2Du <eduardohilario.eha@gmail.com>
2022-05-08 10:23:37 -0300

    Criando projeto

---
+++ src/main/java/eduardo/BusTime/BusTimeApplication.java
@@ -0,0 +1,13 @@
+package eduardo.BusTime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusTimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eduardo/BusTime/BusTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package eduardo.BusTime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
