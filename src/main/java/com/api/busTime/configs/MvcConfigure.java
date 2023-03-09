68489174da096e698525a2011f8de39bde35dabb
Du2Du <eduardohilario.eha@gmail.com>
2023-03-09 14:26:07 -0300

    Atualizando Frontend

--- src/main/java/com/api/busTime/configs/MvcConfigure.java
+++ src/main/java/com/api/busTime/configs/MvcConfigure.java
@@ -7,21 +7,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MvcConfigure implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/home").setViewName("Home/home");
-        registry.addViewController("/").setViewName("Dashboard/index");
-        registry.addViewController("/favorites").setViewName("Favorites/favorites");
-        registry.addViewController("/permissions").setViewName("Permissions/permissions");
-        registry.addViewController("/permissions-list").setViewName("PermissionsList/permissions-list");
-        registry.addViewController("/login").setViewName("Login/login");
-        registry.addViewController("/register").setViewName("Register/register");
-        registry.addViewController("/bus").setViewName("Bus/bus");
-        registry.addViewController("/create-bus").setViewName("CreateBus/create-bus");
-        registry.addViewController("/logout").setViewName("Logout/logout");
-        registry.addViewController("/profile").setViewName("Profile/profile");
-        registry.addViewController("/search-bus").setViewName("SearchBus/search-bus");
-        registry.addViewController("/update-bus/{id}").setViewName("UpdateBus/update-bus");
-        registry.addViewController("/profile/update").setViewName("UpdateUser/update");
-        registry.addViewController("/statistics").setViewName("Statistics/statistics");
-        registry.addViewController("/logs").setViewName("Logs/logs");
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/favorites").setViewName("favorites");
+        registry.addViewController("/permissions").setViewName("permissions");
+        registry.addViewController("/permissions-list").setViewName("permissions-list");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/register").setViewName("register");
+        registry.addViewController("/bus").setViewName("bus");
+        registry.addViewController("/create-bus").setViewName("create-bus");
+        registry.addViewController("/logout").setViewName("logout");
+        registry.addViewController("/profile").setViewName("profile");
+        registry.addViewController("/search-bus").setViewName("search-bus");
+        registry.addViewController("/update-bus/{id}").setViewName("update-bus");
+        registry.addViewController("/profile/update").setViewName("update");
+        registry.addViewController("/statistics").setViewName("statistics");
+        registry.addViewController("/logs").setViewName("logs");
     }
 }
